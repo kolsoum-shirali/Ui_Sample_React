c00cfafd80693e64b4772fe218b2198599aafbbb
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-23 04:50:31 -0700

    update 1 file and create 1 file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import HomePage from './pages/home/index'
 function App() {
   return (
-    <div className="App">
-    
+    <div className="App bg-Slate-100">
+      <HomePage />
     </div>
   );
 }
