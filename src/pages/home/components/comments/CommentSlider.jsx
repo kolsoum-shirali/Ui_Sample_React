2a70d2ef8143deeafe2ee9948066c592765cbdfb
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-25 04:55:30 -0700

    feat: create CommentSlider.jsx

---
+++ src/pages/home/components/comments/CommentSlider.jsx
@@ -0,0 +1,108 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Autoplay,
+  EffectCoverflow,
+  Pagination,
+} from "swiper/modules";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css";
+
+import SingleComment from "./SingleComment";
+
+const comments = [
+  {
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.",
+    author: "مهتاب سلامی",
+  },
+  {
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است  چاپگرها و متون بلکه روزنامه و چاپگرها و متون بلکه روزنامه و  چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.",
+    author: "مهتاب سلامی",
+  },
+  {
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.",
+    author: "مهتاب سلامی",
+  },
+  {
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است  چاپگرها و متون بلکه روزنامه و چاپگرها و متون بلکه روزنامه و  چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.",
+    author: "مهتاب سلامی",
+  },
+  {
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.",
+    author: "مهتاب سلامی",
+  },
+  {
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها  چاپگرها و متون بلکه روزنامه و چاپگرها و متون بلکه روزنامه و چاپگرها و متون بلکه روزنامه و چاپگرها و متون بلکه روزنامه وو متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.",
+    author: "مهتاب سلامی",
+  },
+  {
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.",
+    author: "مهتاب سلامی",
+  },
+  {
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها  چاپگرها و متون بلکه روزنامه و چاپگرها و متون بلکه روزنامه و چاپگرها و متون بلکه روزنامه و چاپگرها و متون بلکه روزنامه وو متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.",
+    author: "مهتاب سلامی",
+  },
+];
+
+export default function CommentSlider() {
+  return (
+    <div>
+      <h1 className="text-center text-2xl lg:text-4xl">نظرات مشتری ها</h1>
+      <div className="relative">
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          navigation={{
+            nextEl: "#next-btn",
+            prevEl: "#prev-btn",
+          }}
+          pagination={{
+            type: "bullets",
+            clickable: true,
+          }}
+          modules={[Navigation, Autoplay, EffectCoverflow, Pagination]}
+          className="mt-5 lg:mt-10"
+          loop={true}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            992: {
+              slidesPerView: 3,
+            },
+            1024: {
+              slidesPerView: 4,
+            },
+          }}
+        >
+          {comments.map((comment, index) => (
+            <SwiperSlide key={index}>
+              <SingleComment comment={comment} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <div className="hidden xl:block">
+          <i
+            id="prev-btn"
+            className="icon-right-open text-4xl absolute -right-16 top-1/2 z-1 text-green-600 cursor-pointer"
+          ></i>
+          <i
+            id="next-btn"
+            className="icon-left-open text-4xl absolute -left-16 top-1/2 z-1 text-green-600 cursor-pointer"
+          ></i>
+        </div>
+      </div>
+    </div>
+  );
+}
