1fee9a46f9622f8715f96dcd8268aa9312494674
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-26 13:05:02 -0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -20,12 +20,11 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
         {/* Routes without Layout */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {/* Not Found Route */}
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
