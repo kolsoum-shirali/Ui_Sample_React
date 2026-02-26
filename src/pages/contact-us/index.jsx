69477fac4f24e5a6269cbae3beab6278d443d26a
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-26 11:14:55 -0700

    update src/pages/contact-us/index.jsx

--- src/pages/contact-us/index.jsx
+++ src/pages/contact-us/index.jsx
@@ -1,5 +1,7 @@
 import BreadCrumbsBanner from "../../components/common/BreadCrumbsBanner";
 import ContactWays from "./components/ContactWays";
+import ContactComment from "./components/ContactComment";
+
 const options = [
   { title: "خانه", link: "/" },
   { title: "ارتباط با ما", link: "" },
@@ -11,6 +13,7 @@ export default function ContactUs() {
       <BreadCrumbsBanner options={options} caption="ارتباط با ما" />
       <div className="container mx-auto p-5 mt-7 md:mt-16 space-y-10 lg:space-y-32">
         <ContactWays />
+        <ContactComment />
       </div>
     </div>
   );
