26f60154067095a3d0716f2c3d802e70df0137ce
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-27 10:47:56 -0700

    update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import TopHeader from "./TopHeader";
 import DesktopMenubar from "./DesktopMenubar";
+import MobileMenubar from "./MobileMenubar";
+
 export default function Header() {
+  const [isMobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuVisible(!isMobileMenuVisible);
+  };
+
   return (
     <div className="shadow-md">
       <div className="container mx-auto">
-        <TopHeader />
+        <div className="relative max-lg:mb-24">
+        <TopHeader toggleMobileMenu={toggleMobileMenu} />
+        </div>
         <DesktopMenubar />
+        <MobileMenubar
+          closeMenu={toggleMobileMenu}
+          menuShow={isMobileMenuVisible}
+        />
       </div>
     </div>
   );
