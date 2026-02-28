d81fcfe6d5bdf9b5ea0d62235153d8be7e81d382
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-28 01:46:05 -0700

    update ProductComments.jsx

--- src/pages/products/components/ProductComments.jsx
+++ src/pages/products/components/ProductComments.jsx
@@ -19,5 +19,9 @@ const comments = [
 ];
 
 export default function ProductComments() {
-  return <CommentsList comments={comments} />;
+  return (
+    <div className="shadow-md rounded-md p-5 md:p-10 border border-gray-400/20">
+      <CommentsList comments={comments} />
+    </div>
+  );
 }
