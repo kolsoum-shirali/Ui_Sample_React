c85e87db23a7904578d71bf59710eea5035266be
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-28 09:45:13 -0600

    feat: create PagesItems.jsx

---
+++ src/components/layout/PagesItems.jsx
@@ -0,0 +1,25 @@
+const options = [
+  { title: "Blog" },
+  { title: "Browse the Shop" },
+  { title: "Category" },
+  { title: "Pre-Built Pages" },
+  { title: "Visual Composer Elements" },
+  { title: "WooCommerce Pages" },
+];
+
+export default function PagesItems() {
+  return (
+    <div className="lg:col-span-1">
+      <h3 className="text-lg lg:text-xl mb-2 md:mb-7">Pages</h3>
+      <div className="space-y-4">
+        {options.map((option, index) => {
+          return (
+            <div key={index}>
+              <span className="text-sm lg:text-base text-gray-400">{option.title}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
