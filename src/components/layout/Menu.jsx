c25b039e48061de411f090b1ed915a3895f6e6f6
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-28 05:59:31 -0600

    feat: create Menu.jsx

---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+
+export default function HeaderMenu({ menuOptions }) {
+  return (
+    <ul className="flex gap-x-6">
+      {menuOptions.map((menu, index) => {
+        return (
+          <li
+            key={index}
+            className="relative hover:text-purple-900 transition-colors bg cursor-pointer group"
+          >
+            {menu.url ? (
+              <Link to={menu.url}>
+                {menu.title} <i className={menu.icon}></i>
+              </Link>
+            ) : (
+              <span className={menu.active ? "text-mainPink" : ""}>
+                {menu.title}
+              </span>
+            )}
+            {menu.subMenu.length > 0 && (
+              <i
+                className={`icon-down-open ${menu.active ? "text-mainPink" : ""}`}
+              ></i>
+            )}
+            {menu.subMenu.length > 0 && (
+              <div className="absolute right-0 w-56 bg-white opacity-0 border border-gray-400/20 shadow-md z-10 p-5 invisible transition-all transform translate-y-16 group-hover:translate-y-0 duration-300 ease-in-out delay-150 group-hover:opacity-100 group-hover:visible">
+                <div>
+                  {menu.subMenu.map((item, inx) => {
+                    return (
+                      <Link key={inx} to={item.url} className="text-sm">
+                        <h6 className="text-gray-500 hover:bg-slate-100 p-2 hover:text-purple-900 transition-colors cursor-pointer ">
+                          {item.title}
+                        </h6>
+                      </Link>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
