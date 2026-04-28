05fbf03e097cf71bf6bf5117dfb9893a011cf3ad
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-28 05:59:39 -0600

    update HeaderMenu.jsx

--- src/components/layout/HeaderMenu.jsx
+++ src/components/layout/HeaderMenu.jsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
 import Logo from "../../assets/img/logo.png";
-const Menus = [
+import Menu from "./Menu";
+const menuOptions = [
   {
     title: "Home",
     url: "",
@@ -9,6 +9,7 @@ const Menus = [
       { title: "contact_us", url: "/" },
       { title: "about_us", url: "/" },
     ],
+        active:true
   },
   { title: "Pages", url: "/", subMenu: [] },
 
@@ -24,42 +25,7 @@ export default function HeaderMenu() {
       <div className="container mx-auto grid grid-cols-2 ">
         <div className="col-span-1 flex items-end">
           <img src={Logo} alt="main-logo" className="mr-10" />
-          <ul className="flex gap-x-6">
-            {Menus.map((menu, index) => {
-              return (
-                <li
-                  key={index}
-                  className="relative hover:text-purple-900 transition-colors bg cursor-pointer group"
-                >
-                  {menu.url ? (
-                    <Link to={menu.url}>
-                      {menu.title} <i className={menu.icon}></i>
-                    </Link>
-                  ) : (
-                    <span className="text-mainPink">{menu.title}</span>
-                  )}
-                  {menu.subMenu.length > 0 && (
-                    <i className="icon-down-open text-mainPink"></i>
-                  )}
-                  {menu.subMenu.length > 0 && (
-                    <div className="absolute right-0 w-56 bg-white opacity-0 border border-gray-400/20 shadow-md z-10 p-5 invisible transition-all transform translate-y-16 group-hover:translate-y-0 duration-300 ease-in-out delay-150 group-hover:opacity-100 group-hover:visible">
-                      <div>
-                        {menu.subMenu.map((item, inx) => {
-                          return (
-                            <Link key={inx} to={item.url} className="text-sm">
-                              <h6 className="text-gray-500 hover:bg-slate-100 p-2 hover:text-purple-900 transition-colors cursor-pointer ">
-                                {item.title}
-                              </h6>
-                            </Link>
-                          );
-                        })}
-                      </div>
-                    </div>
-                  )}
-                </li>
-              );
-            })}
-          </ul>
+          <Menu menuOptions={menuOptions} />
         </div>
         <div className="col-span-1 flex justify-end">
           <input
