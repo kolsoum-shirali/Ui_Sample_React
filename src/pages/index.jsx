a1064299e4b4c3113c0ce036d635c3aa156f6027
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-28 09:59:12 -0600

    update src/pages/index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,4 @@
-//Better way of importing
-
 export { default as HomePage } from "./home";
-export { default as Login } from "./login";
-export { default as Register } from "./register";
-export { default as AboutUs } from "./about-us";
-export { default as ContactUs } from "./contact-us";
-export { default as NotFound } from "./notFound";
-export { default as BlogPage } from "./blog";
-export { default as Profile } from "./profile";
-export { default as BlogDetail } from "./blog/BlogDetail";
-export { default as CreateAds } from "./ads/CreateAds";
-
-export { default as ProductsDetail } from "./products/ProductsDetail";
-
-
 
 /* -- Layout -- */
 export { default as Footer } from "../components/layout/Footer";
