ffef64d000f7f4c53dd002d7bfa0453426529325
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-28 09:45:53 -0600

    feat: create CustomerCare.jsx

---
+++ src/components/layout/CustomerCare.jsx
@@ -0,0 +1,24 @@
+const options = [
+  { title: "MyAccount" },
+  { title: "Discount" },
+  { title: "Returns" },
+  { title: "Orders History" },
+  { title: "Orders Tracking" },
+];
+
+export default function CustomerCare() {
+  return (
+    <div className="lg:col-span-1">
+      <h3 className="text-lg lg:text-xl mb-2 md:mb-7">Customer Care</h3>
+      <div className="space-y-4">
+        {options.map((option, index) => {
+          return (
+            <div key={index}>
+              <span className="text-sm lg:text-base text-gray-400">{option.title}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
