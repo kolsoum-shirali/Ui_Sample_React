3875af27f255b16f7b947880e1240e9f2c499f71
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-28 09:59:04 -0600

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import { Route, Routes, Outlet } from "react-router-dom";
 import {
   HomePage,
-  Login,
-  Register,
   Footer,
-  Header,
-  NotFound,
-  AboutUs,
-  ContactUs,
-  BlogPage,
-  BlogDetail,
-  Profile,
-  ProductsDetail,
-  CreateAds,
+  Header
 } from "./pages/index";
 import "./App.css";
 
@@ -22,17 +12,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/blog/:id" element={<BlogDetail />} />
-          <Route path="/products/:id" element={<ProductsDetail />} />
-          <Route path="/create-ads" element={<CreateAds />} />
-          <Route path="*" element={<NotFound />} />
         </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
