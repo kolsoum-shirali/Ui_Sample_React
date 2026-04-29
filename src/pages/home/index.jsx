2b8b6523ea8949436104f9b915231c024bf39794
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-29 01:57:38 -0600

    update src/pages/home/index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
 import HeaderSlider from "./components/headerSlider/HeaderSlider";
 import LatestBlog from "./components/blog/LatestBlog";
+import BrandList from "./components/brand/BrandList";
 const HomePage = () => {
   return (
     <div className="space-y-5 md:space-y-20">
       <HeaderSlider />
+      <BrandList />
       <LatestBlog />
     </div>
   );
