3d8d05b7570a2759bd3ea2ced0f7a0eb1cf52f0c
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-29 15:21:13 -0600

    update src/pages/home/index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,10 +4,12 @@ import BrandList from "./components/brand/BrandList";
 import Banner from "./components/banner/‌Banner";
 import TopCategories from "./components/categories/TopCategories";
 import TrendingProducts from "./components/products/trend/TrendingProducts";
+import FeatureProducts from './components/products/feature/FeatureProducts'
 const HomePage = () => {
   return (
     <div className="space-y-5 md:space-y-20">
       <HeaderSlider />
+      <FeatureProducts/>
       <TrendingProducts />
       <TopCategories />
       <Banner />
