e21b026ee7c59d5b8db3aea299bbdc4642abaa18
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-30 00:31:31 -0600

    update BannerCard.jsx

--- src/pages/home/components/headerSlider/BannerCard.jsx
+++ src/pages/home/components/headerSlider/BannerCard.jsx
@@ -4,27 +4,27 @@ import pic2 from "../../../../assets/img/pic-2.png";
 export default function BannerCard() {
   return (
     <div className="bg-lavender/50">
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 p-5 md:p-0">
         <div className="col-span-2 md:col-span-1">
           <div className="grid grid-cols-12 h-full">
-            <div className="col-span-4 max-h-[350px]  pr-5 flex justify-end overflow-hidden">
+            <div className="col-span-4 max-h-[350px]  pr-5 hidden md:flex justify-end overflow-hidden ">
               <img
                 src={pic2}
                 alt={pic2 || "Banner Image2"}
                 className="max-w-full h-auto object-contain"
               />
             </div>
-            <div className="col-span-8 h-full flex flex-col justify-center items-start space-y-4">
+            <div className="col-span-12 md:col-span-8 h-full flex flex-col justify-center items-start space-y-2 md:space-y-4">
               <h6 className="text-mainPink font-semibold text-sm">
                 Best Furniture For Your Castle...
               </h6>
-              <h1 className="font-bold text-5xl">New Furniture Collection</h1>
-              <h1 className="font-bold text-5xl">Trends in 2020</h1>
+              <h1 className="font-bold text-2xl lg:text-5xl">New Furniture Collection</h1>
+              <h1 className="font-bold text-2xl lg:text-5xl">Trends in 2020</h1>
               <p className="text-sm text-gray-400 font-semibold">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
                 nisi natus amet accusamus repellendus facilis in!
               </p>
-              <button className="bg-mainPink text-white px-7 py-3 rounded text-sm">
+              <button className="bg-mainPink text-white px-5 md:px-7 py-3 rounded text-sm">
                 Shop Now
               </button>
             </div>
