444e20eb16045cccc65a676ac56dc43c611ebd75
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-29 19:02:05 -0600

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const { purple } = require('@mui/material/colors')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -7,10 +5,14 @@ module.exports = {
     extend: {
       colors: {
         mainPurple: "#7C32DA",
-        mainPink:"#FC2D85",
-        ghostWhite:"#EDEFFB",
-        lavender:"#E6E3F9",
-        liberty:"#61629F"
+        mainPink: "#FC2D85",
+        ghostWhite: "#EDEFFB",
+        lavender: "#E6E3F9",
+        liberty: "#181B76",
+        mediumBlue: "#301AC4",
+      },
+      zIndex: {
+        1: "1",
       },
     },
   },
